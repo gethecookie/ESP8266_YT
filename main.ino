52e06b1a05f2b65827d48d9915b3b1768115c77f
cioccarellia <andrea.cioccarelli01@gmail.com>
2021-01-27 01:09:40 +0100

    Fixed freaking indentation to 4spaces

--- main.ino
+++ main.ino
@@ -16,11 +16,11 @@
 
 //Parametri per connessione a rete WiFi
 char ssid[]              = "**********";  // SSID Rete Wi-Fi
-char password[]          = "**********";  // Password di Wi-Fi
+char password[]          = "**********";  // Password della Wi-Fi
 
 //Parametri per comunicazione con YouTube
-#define idCanale          "**********"    // ID Canale YouTube
-#define apiKey            "**********"    // Api Key Google
+#define idCanale          "**********"    // ID Canale YouTube (Quello nell'URL)
+#define apiKey            "**********"    // Chiave API di Google
 
 WiFiClientSecure client;
 YoutubeApi api(apiKey, client);
@@ -29,51 +29,51 @@ MD_Parola displayLed = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_D
 
 void setup(void) {
 
-  client.setInsecure();
+	client.setInsecure();
   
-  displayLed.begin();  //inizializzo matrice led
+	displayLed.begin();  //inizializzo matrice led
 
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
+	WiFi.mode(WIFI_STA);
+	WiFi.disconnect();
+	delay(100);
   
 /*
-  Serial.begin(9600); //inizializzo scrittura di verifica a console
-  Serial.print("Connessione in corso a: ");
-  Serial.println(ssid);
+	Serial.begin(9600); //inizializzo scrittura di verifica a console
+	Serial.print("Connessione in corso a: ");
+	Serial.println(ssid);
 
-  api._debug = true;  //stampa in automatico info di debug
+	api._debug = true;  //stampa in automatico info di debug
 */
 
-  WiFi.begin(ssid,password);  //mi connetto alla rete
+	WiFi.begin(ssid,password);  //mi connetto alla rete
 
-  while(WiFi.status() != WL_CONNECTED){
-      delay(500);
-      //Serial.print(".");        
-  }
+	while(WiFi.status() != WL_CONNECTED){
+	    delay(500);
+	    //Serial.print(".");        
+	}
 
 /*
-  Serial.println();
+	Serial.println();
 
-  Serial.println("[DEBUG] Connessione avvenuta con successo!");
-  Serial.print("NodeMCU Indirizzo IP: ");
-  Serial.println(WiFi.localIP());
+	Serial.println("[DEBUG] Connessione avvenuta con successo!");
+	Serial.print("NodeMCU Indirizzo IP: ");
+	Serial.println(WiFi.localIP());
 */
 
-  displayLed.setTextAlignment(PA_CENTER);  //allineo testo al centro
+	displayLed.setTextAlignment(PA_CENTER);  //allineo testo al centro
 }
 
 void loop(void) {
-  if(api.getChannelStatistics(idCanale)) {
-    /*
-      Serial.print("Numero Iscritti: ");
-      Serial.println(api.channelStats.subscriberCount);
-    */
-      displayLed.print(api.channelStats.subscriberCount);
-  } 
-  else 
-    displayLed.print("Error");
+	if(api.getChannelStatistics(idCanale)) {
+	/*
+	  Serial.print("Numero Iscritti: ");
+	  Serial.println(api.channelStats.subscriberCount);
+	*/
+		displayLed.print(api.channelStats.subscriberCount);
+	} else {
+    	displayLed.print("Error");
+	}
   
   
-  delay(1000 * 60 * 30);  // aggiorno ogni 30min
+  	delay(1000 * 60 * 30);  // aggiorno ogni 30min
 }
